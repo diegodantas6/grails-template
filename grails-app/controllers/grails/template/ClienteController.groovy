da27aebcaa20896515f5b5bccbd2a37e4d5b866d
diego.dantas <diegodantas6@gmail.com>
2016-03-11 14:27:48 -0300

    teste de segurança

--- grails-app/controllers/grails/template/ClienteController.groovy
+++ grails-app/controllers/grails/template/ClienteController.groovy
@@ -1,5 +1,8 @@
 package grails.template
 
+import grails.plugin.springsecurity.annotation.Secured;
+
+@Secured("IS_AUTHENTICATED_ANONYMOUSLY")
 class ClienteController {
 
     def index() { }
---
+++ grails-app/controllers/grails/template/ProdutoController.groovy
@@ -0,0 +1,6 @@
+package grails.template
+
+class ProdutoController {
+
+    def index() { }
+}
